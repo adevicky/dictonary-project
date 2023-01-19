8ad3abb3fb178dd45020e616b2e3ea8b05019fde
Adenuga Victoria <adenugavictoria29@gmail.com>
2023-01-19 08:44:54 +0000

    Created Dictionary.js component

--- my-dict-react/src/App.js
+++ my-dict-react/src/App.js
@@ -1,14 +1,19 @@
-import logo from './logo.png';
-import './App.css';
+import logo from "./logo.png";
+import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default  function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="text-center">Coded by VicCodes</footer>
+      </div>
     </div>
   );
 }
-
- App;
